fca09c919ecee750775f49dcbf9d6c5b109663eb
yejinzhan <jinzhan.ye@zhenai.com>
2022-09-06 20:38:11 +0800

    chore: update

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const KoaRouter = require('@koa/router')
 const multer = require('@koa/multer')
 const sendFile = require('koa-sendfile')
 const path = require('path')
+const fs = require('fs')
+const mime = require('mime')
+const staticServe = require('koa-static')
 
 const isServerless = process.env.SERVERLESS
 const app = new Koa()
 const router = new KoaRouter()
 const upload = multer({ dest: isServerless ? '/tmp/upload' : './upload' })
+
+// or use absolute paths
+app.use(staticServe(__dirname + '/static'))
 
 router.get(`/`, async (ctx) => {
   await sendFile(ctx, path.join(__dirname, 'index.html'))
@@ -18,6 +24,16 @@ router.post('/upload', upload.single('file'), (ctx) => {
     success: true,
     data: ctx.file
   }
+})
+
+router.get('/image', (ctx) => {
+  const filePath = path.join(__dirname, './static/luobo-avatar.jpg');
+  const file = fs.readFileSync(filePath); //读取文件
+
+  let mimeType = mime.getType(filePath); //读取图片文件类型
+  ctx.set('content-type', mimeType); //设置返回类型
+  ctx.set('Content-disposition', 'attachment; filename=' + 'luobo-avatar.jpg');// 设置浏览器以附件的形式下载并保存到本地
+  ctx.body = file; //返回图片
 })
 
 app.use(router.routes()).use(router.allowedMethods())
