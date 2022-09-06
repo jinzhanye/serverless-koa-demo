4ddde3d638888f27794d87a08db1e46bd8a42e81
yejinzhan <jinzhan.ye@zhenai.com>
2022-09-06 15:00:32 +0800

    chore: update

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const Koa = require('koa')
-const KoaRouter = require('koa-router')
+const KoaRouter = require('@koa/router')
+const multer = require('@koa/multer')
 const sendFile = require('koa-sendfile')
 const path = require('path')
-const multer = require('@koa/multer');
 
+const isServerless = process.env.SERVERLESS
 const app = new Koa()
 const router = new KoaRouter()
+const upload = multer({ dest: isServerless ? '/tmp/upload' : './upload' })
 
-// Serverless 场景只能读写 /tmp 目录，所以这里需要指定上传文件的目录为 /tmp/upload
-const upload = multer({ dest: '/tmp/upload' });
-
-// Routes
 router.get(`/`, async (ctx) => {
   await sendFile(ctx, path.join(__dirname, 'index.html'))
 })
@@ -18,13 +16,16 @@ router.get(`/`, async (ctx) => {
 router.post('/upload', upload.single('file'), (ctx) => {
   ctx.body = {
     success: true,
-    data: ctx.file,
-  };
-});
-
-app.use(router.allowedMethods()).use(router.routes())
-
-app.listen(9000, () => {
-  console.log(`Server start on http://localhost:9000`);
+    data: ctx.file
+  }
 })
 
+app.use(router.routes()).use(router.allowedMethods())
+
+if (isServerless) {
+  module.exports = app
+} else {
+  app.listen(3010, () => {
+    console.log(`Server start on http://localhost:3010`)
+  })
+}
